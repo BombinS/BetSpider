c17461d5ff0e5cf56e400f4ff47268333aace7b1
Бомбин Сергей Викторович <U_025K1@regions.alfaintra.net>
2026-03-14 12:05:52 +0300

    Вероятности в процентном формате

--- labs/get_prohability.py
+++ labs/get_prohability.py
@@ -4,18 +4,26 @@ def getProhability(k):
     result = []
     if len(k) == 2:
         all = 1/k[0] + 1/k[1]
-        result.append(1/k[0]/all) 
-        result.append(1/k[1]/all)
-        result.append(1/all)
+        result.append(round(1/k[0]/all*100,1)) 
+        result.append(round(1/k[1]/all*100,1))
+        result.append(round(1/all*100,1))
     if len(k) == 3:
         all = 1/k[0] + 1/k[1] + 1/k[2]
-        result.append(1/k[0]/all) 
-        result.append(1/k[1]/all)
-        result.append(1/k[2]/all)
-        result.append(1/all)
+        result.append(round(1/k[0]/all*100,1)) 
+        result.append(round(1/k[1]/all*100,1))
+        result.append(round(1/k[2]/all*100,1))
+        result.append(round(1/all*100,1))
     return result
 
-print (getProhability([1.9, 1.9]))
-print (getProhability([9, 5.3, 1.35]))
-print (getProhability([1.85, 3.8, 4.5]))
-print (getProhability([3, 3.45, 2.35]))
+print (getProhability([3.30, 1.33]))
+print (getProhability([9.00, 5.40, 1.33]))
+print()
+print (getProhability([1.85, 2.00]))
+print (getProhability([1.85, 3.80, 4.5]))
+print()
+print (getProhability([1.62, 2.35]))
+print (getProhability([3.00, 3.45, 2.35]))
+print()
+print (getProhability([1.42, 2.95]))
+print (getProhability([2.50, 3.20, 2.95]))
+print()
