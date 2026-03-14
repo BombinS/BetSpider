4a0d225fc8438c73c96c8b58cf4ee319caf0f8ab
Бомбин Сергей Викторович <U_025K1@regions.alfaintra.net>
2026-03-14 10:21:52 +0300

    Добавил расчет маржи

--- labs/get_prohability.py
+++ labs/get_prohability.py
@@ -6,14 +6,16 @@ def getProhability(k):
         all = 1/k[0] + 1/k[1]
         result.append(1/k[0]/all) 
         result.append(1/k[1]/all)
+        result.append(1/all)
     if len(k) == 3:
         all = 1/k[0] + 1/k[1] + 1/k[2]
         result.append(1/k[0]/all) 
         result.append(1/k[1]/all)
         result.append(1/k[2]/all)
+        result.append(1/all)
     return result
 
-print (getProhability([1.85, 1.85]))
-print (getProhability([9, 5.2, 1.35]))
+print (getProhability([1.9, 1.9]))
+print (getProhability([9, 5.3, 1.35]))
 print (getProhability([1.85, 3.8, 4.5]))
 print (getProhability([3, 3.45, 2.35]))
