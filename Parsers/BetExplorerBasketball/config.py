42506a11f70894eeb600572abd9f9be59dc8161a
Бомбин Сергей Викторович <sergey.bombin.1977@gmail.com>
2026-03-22 14:29:28 +0300

    Немного определений бескетбольных сезонов

--- Parsers/BetExplorerBasketball/config.py
+++ Parsers/BetExplorerBasketball/config.py
@@ -2,5 +2,7 @@ target = "acb-2024-2025"
 
 links = {
     "China": ["cba", "cba-2024-2025", "cba-2023-2024"],
-    "Spain": ["acb", "acb-2024-2025", "acb-2023-2024"]
+    "Spain": ["acb", "acb-2024-2025", "acb-2023-2024"],
+    "Germany": ["bbl", "bbl-2024-2025", "bbl-2023-2024"],
+    "France": ["lnb", "lnb-2024-2025", "lnb-2023-2024"]
 }
