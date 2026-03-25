04f17073799a4f44615d4db28d803547c7594de8
Бомбин Сергей Викторович <sergey.bombin.1977@gmail.com>
2026-03-25 19:21:20 +0300

    Проба создания таблицы

--- DB/db.py
+++ DB/db.py
@@ -1,8 +1,12 @@
 #!/bin/usr/env python
 
 import psycopg2
+from contextlib import closing
+import logging
+logging.basicConfig(filename="dd.log", filemode="w", level=logging.DEBUG, encoding="utf-8")
 
-def InitConnection():
+# подключение к Pg
+def init_connection():
     conn = psycopg2.connect(
         host = "localhost",
         port = "5432",
@@ -12,6 +16,24 @@ def InitConnection():
     )
     return conn
 
+# создание таблицы баскетбольной статистики
+def create_table_basketball(conn):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS table_01
+                (
+                    id SERIAL PRIMARY KEY,
+                    homeTeam VARCHAR(100),
+                    awayTeam VARCHAR(100)
+                )    
+                """
+            )
+            conn.commit()
+        except Exception as e:
+            logging.error(e)
+
 if __name__ == "__main__":
-    conn = InitConnection()
-    conn.close()
+    with closing(init_connection()) as conn:
+        create_table_basketball(conn)
