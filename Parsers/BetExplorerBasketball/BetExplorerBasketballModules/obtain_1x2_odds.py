39626f41d64b8b424cd86a228724005c30f4e05c
Бомбин Сергей Викторович <sergey.bombin.1977@gmail.com>
2026-03-28 15:50:50 +0300

    Обработка нулового коэффициента

--- Parsers/BetExplorerBasketball/BetExplorerBasketballModules/obtain_1x2_odds.py
+++ Parsers/BetExplorerBasketball/BetExplorerBasketballModules/obtain_1x2_odds.py
@@ -7,7 +7,8 @@ class Obtain1X2Odds:
         self.matchInfo = matchInfo
         self.xpath1X2 = "//a[@class='list-tabs__item__in current' and text()='1X2']"
         self.xpathRaw = "//tr[@class=' odd' or @class=' even']"
-        self.xpathOddCell = "//td[@data-odd!='']"
+        # self.xpathOddCell = "//td[@data-odd!='']"
+        self.xpathOddCell = "//td"
         self.timeout = 5000
 
     def process(self):
@@ -21,9 +22,9 @@ class Obtain1X2Odds:
             rows = self.page.locator(f"xpath={self.xpathRaw}")
             for i in range(rows.count()):
                 cells = rows.nth(i).locator(self.xpathOddCell)
-                self.matchInfo["oddHomeWin"].append(cells.nth(0).inner_text())
-                self.matchInfo["oddDraw"].append(cells.nth(1).inner_text())
-                self.matchInfo["oddAwayWin"].append(cells.nth(2).inner_text())
+                self.matchInfo["oddHomeWin"].append(cells.nth(3).inner_text())
+                self.matchInfo["oddDraw"].append(cells.nth(4).inner_text())
+                self.matchInfo["oddAwayWin"].append(cells.nth(5).inner_text())
             self.matchInfo["oddHomeWin"] = self.getAvg(self.matchInfo["oddHomeWin"])
             self.matchInfo["oddDraw"] = self.getAvg(self.matchInfo["oddDraw"])
             self.matchInfo["oddAwayWin"] = self.getAvg(self.matchInfo["oddAwayWin"])
@@ -38,5 +39,11 @@ class Obtain1X2Odds:
     def getAvg(self, odds):
         sum = 0.0
         for i in range(len(odds)):
-            sum = sum + float(odds[i])
+            odd = odds[i]
+            try:
+                odd = float(odd)
+            except:
+                odd = 0.0
+            sum = sum + odd
         return round(sum/len(odds), 2)
+    
