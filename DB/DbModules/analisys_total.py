93262b76567b1aa9d17e3f4489da857722ebb4a4
Бомбин Сергей Викторович <sergey.bombin.1977@gmail.com>
2026-03-30 18:52:12 +0300

    Получение необработанных баскетбольных тоталов

--- DB/DbModules/analisys_total.py
+++ DB/DbModules/analisys_total.py
@@ -1,4 +1,5 @@
 from . db_enum import DbEnum
+import logging
 
 class AnalisysTotal:
 
@@ -15,7 +16,15 @@ class AnalisysTotal:
         # запрос на получение идентификторов необработанных матчей 
         # каких id из ftbasketballresults нет в качестве 'sourceMatchId' в ftbasketballanalisystotal
         try:
-            pass
-        except:
-            pass
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    """
+                    SELECT t1.id FROM ftbasketballresults as t1
+                    LEFT JOIN ftbasketballanalisystotal as t2 on t1.id = t2.sourcematchid
+                    WHERE T2.sourcematchid is NULL
+                    """
+                )
+                results = cur.fetchall()
+        except Exception as e:
+            logging.error(f"analisys_total_basketball #1: {e}")
     
